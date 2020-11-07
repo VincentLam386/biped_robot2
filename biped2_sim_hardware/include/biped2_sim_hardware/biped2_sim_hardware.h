6a5dd9cd7009d35047334827f23128880255ea26
VincentLam386 <tclamai@connect.ust.hk>
2020-11-07 11:14:01 +0800

    Add simulation HW skeleton

---
+++ biped2_sim_hardware/include/biped2_sim_hardware/biped2_sim_hardware.h
@@ -0,0 +1,13 @@
+#include <gazebo_ros_control/default_robot_hw_sim.h>
+
+namespace biped2_sim_hw_ns
+{
+
+class BipedSimHW: public gazebo_ros_control::DefaultRobotHWSim
+{
+public:
+  void exampleFunc();
+
+protected:
+
+};
---
+++ biped2_sim_hardware/src/biped_sim_hardware.cpp
@@ -0,0 +1,18 @@
+#include <biped2_sim_hardware>
+
+namespace biped2_sim_hw_ns
+{
+
+void BipedSimHW::exampleFunc(){
+  // example func
+  // ...
+  // ...
+  //
+
+}	
+
+
+
+}
+
+PLUGINLIB_EXPORT_CLASS(biped2_sim_hw_ns::BipedSimHW, gazebo_ros_control::DefaultRobotHWSim)
